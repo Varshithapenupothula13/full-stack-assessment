90049437c4f48aebf776aee1af607e4ee423cb57
Varshitha Penupothula <Penupothulavarshitha@gmail.com>
2026-07-15 19:18:18 +0530

    created dashboard component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import "./login.css";
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="login-container">
+      <div className="login-card">
+        <h1>Welcome 🎉</h1>
+        <p>You have successfully logged in.</p>
+
+        <button
+          className="login-btn"
+          onClick={() => navigate("/dashboard")}
+        >
+          Go to Dashboard
+        </button>
+      </div>
     </div>
   );
 }
