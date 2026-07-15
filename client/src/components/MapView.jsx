9a3c0a52c6b5ac2c438441f8a680b3a0f0d25533
Varshitha Penupothula <Penupothulavarshitha@gmail.com>
2026-07-16 01:34:11 +0530

    added map route visualization

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -1,15 +1,25 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function MapView() {
+  // Pickup & Drop Coordinates
   const pickup = [17.0005, 81.8040]; // Rajahmundry
   const drop = [16.9891, 82.2475]; // Kakinada
+
+  // Route Line
+  const route = [pickup, drop];
 
   return (
     <div
       style={{
         width: "100%",
-        height: "500px",
+        height: "100%",
         borderRadius: "10px",
         overflow: "hidden",
       }}
@@ -17,20 +27,34 @@ function MapView() {
       <MapContainer
         center={pickup}
         zoom={10}
-        style={{ width: "100%", height: "100%" }}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
       >
         <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
+          attribution="&copy; OpenStreetMap contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
+        {/* Pickup Marker */}
         <Marker position={pickup}>
-          <Popup>Pickup Location</Popup>
+          <Popup>📍 Pickup Location</Popup>
         </Marker>
 
+        {/* Drop Marker */}
         <Marker position={drop}>
-          <Popup>Drop Location</Popup>
+          <Popup>🏁 Drop Location</Popup>
         </Marker>
+
+        {/* Route */}
+        <Polyline
+          positions={route}
+          pathOptions={{
+            color: "blue",
+            weight: 5,
+          }}
+        />
       </MapContainer>
     </div>
   );
